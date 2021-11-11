d73276444aa3c013e8deb06d39503c55b6996bee
JamaCodes <jama.avwork@gmail.com>
2021-11-11 15:26:05 -0600

    testing

---
+++ src/index.js
@@ -0,0 +1,55 @@
+// React Required
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+// Create Import File
+import './index.scss';
+
+import PageScrollTop from './component/PageScrollTop';
+
+// Home layout
+
+
+// Dark Home Layout 
+
+import DarkPortfolioLanding from './dark/PortfolioLanding';
+
+// Element Layout
+
+import Contact from "./elements/Contact";
+import PortfolioDetails from "./elements/PortfolioDetails";
+
+import error404 from "./elements/error404";
+
+
+// Blocks Layout
+
+
+import { BrowserRouter, Switch, Route  } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
+
+
+class Root extends Component{
+    render(){
+        return(
+            <BrowserRouter basename={'/'}>
+                <PageScrollTop>
+                    <Switch>
+                        <Route exact path={'/'} component={DarkPortfolioLanding}/>
+                      
+                        
+                   
+                        <Route exact path={`${process.env.PUBLIC_URL}/portfolio-details`} component={PortfolioDetails}/>
+                        <Route path={`${process.env.PUBLIC_URL}/404`} component={error404}/>
+                        <Route component={error404}/>
+                       
+
+                    </Switch>
+                </PageScrollTop>
+            </BrowserRouter>
+        )
+    }
+}
+
+ReactDOM.render(<Root/>, document.getElementById('root'));
+serviceWorker.register();
